b1d41001b358026629c5f7873c72ae4eb61177b5
estefania-git <ecasmed@gmail.com>
2019-11-20 12:17:48 +0100

    create coasters

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const coaster = require("../models/coaster.model");
+const Parks = require("../models/park.model");
 
 // Aquí los endpoints
+router.get('/', (req, res, next) => {
+    Parks.find()
+        .then(parks => {
+            res.render('coasters/new-coaster', {
+                parks
+            })
+        })
+
+});
+
+
+router.post('/new-coaster', (req, res) => {
+    console.log(req.body)
+    coaster.create(
+            req.body
+        )
+        .then(() => {
+            res.redirect('/')
+        })
+});
 
 
 module.exports = router
--- routes/park.routes.js
+++ routes/park.routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const park = require("../models/park.model");
 
 // Aquí los endpoints
+router.get('/', (req, res, next) => {
+    res.render('parks/new-park')
+
+});
+
+
+router.post('/new-park', (req, res) => {
+    console.log(req.body)
+    park.create({
+            name: req.body.name,
+            description: req.body.description
+
+        })
+        .then(() => {
+            res.redirect('/')
+        })
+});
 
 module.exports = router
